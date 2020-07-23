ae77c8b5bb93f5c24f8212215ca5297dd7c6c253
Andrey_Medwedew <180141@edu.psuti.ru>
2020-07-23 16:35:52 +0400

    Git init.

---
+++ src/main/java/com/medvedev/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 23.7.2020 Andrey Medvedev.
+ */
+
+package com.medvedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
