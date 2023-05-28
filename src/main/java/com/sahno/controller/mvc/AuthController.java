62a456b94d3cc7c830fe52434f1077953864ed4e
amed1020@netcracker.com <andrei.medvedev@netcracker.com>
2023-05-28 14:33:00 +0400

    Sign up visibility

--- src/main/java/com/sahno/controller/mvc/AuthController.java
+++ src/main/java/com/sahno/controller/mvc/AuthController.java
@@ -1,15 +1,21 @@
 package com.sahno.controller.mvc;
 
+import com.sahno.service.VisibilityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
+    @Autowired
+    private VisibilityService visibilityService;
 
     @GetMapping("/sign_in")
-    public String getSignInPage() {
+    public String getSignInPage(Model model) {
+        model.addAttribute("signUpVisibility", visibilityService.calculateSignUpVisibility().getSignUpVisible());
         return "sign_in";
     }
 
